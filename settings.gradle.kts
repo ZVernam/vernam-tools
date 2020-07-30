401fe4c7e72e1be6dee42e36a4efbd4e8cb3fd5f
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2020-07-31 01:50:26 +0300

    load local.properties

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,3 +10,11 @@
 rootProject.buildFileName = "build.gradle.kts"
 rootProject.name = "vernam-tools"
 
+gradle.beforeProject {
+    val localPropertiesFile = rootDir.resolve("local.properties")
+    if (localPropertiesFile.exists()) {
+        val localProperties = java.util.Properties()
+        localProperties.load(localPropertiesFile.inputStream())
+        localProperties.forEach { (k, v) -> if (k is String) project.extra.set(k, v) }
+    }
+}
